633d7e21630183f923c5e2814ff027232c8bb0f8
RolkosModai <74825391+RolkosModai@users.noreply.github.com>
2021-08-12 22:06:33 +0300

    Update attscript.sh

--- Modulos/attscript.sh
+++ Modulos/attscript.sh
@@ -50,12 +50,12 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
 [[ "$vrs1" == "$vrs2" ]] && {
   echo -e " \033[1;36m     SCRIPT ALREADY UPDATED!\033[1;32m\n"
   rm /tmp/att > /dev/null 2>&1
-  echo -e " \033[1;33m MORE INFORMATION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Rolka"
+  echo -e " \033[1;33m MORE INFORMATION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Asrolka"
   echo -ne " \033[1;31m ENTER \033[1;33mto return to \033[1;32mMENU!\033[0m"; read
   menu
 } || {
   echo -e "  \033[1;36mTHERE IS A NEW UPDATE AVAILABLE!\033[1;33m\n"
-  echo -e "  \033[1;33mMORE INFORMATION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Rolka\n"
+  echo -e "  \033[1;33mMORE INFORMATION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Asrolka\n"
   echo -e "  \033[1;32mUPDATE DETAILS:\033[0m\n"
   while read linha; do
     echo -e "  \033[1;37m- \033[1;33m$linha"
